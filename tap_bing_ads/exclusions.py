1a137bfa838c5d5eafcb23b66137632ea9f5c98e
Eli Yarson <eli_tgt@hotmail.com>
2024-10-30 16:32:37 -0300

    add relative ctr back

--- tap_bing_ads/exclusions.py
+++ tap_bing_ads/exclusions.py
@@ -101,6 +101,7 @@ EXCLUSIONS = {
                 "AudienceImpressionLostToBudgetPercent",
                 "AudienceImpressionLostToRankPercent",
                 "AudienceImpressionSharePercent",
+                "RelativeCtr",
             ],
         },
         {

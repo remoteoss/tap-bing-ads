51c562c6bfaa2000b1971cb1e2fcea58d5b66e44
Eli Yarson <eli_tgt@hotmail.com>
2024-10-30 16:23:58 -0300

    fix campaign performance report

--- tap_bing_ads/exclusions.py
+++ tap_bing_ads/exclusions.py
@@ -8,199 +8,195 @@ Docs: https://docs.microsoft.com/en-us/advertising/guides/reports?view=bingads-1
 """
 
 EXCLUSIONS = {
-    'AccountPerformanceReport': [
+    "AccountPerformanceReport": [
         {
-            'Attributes': [
-                'CustomerId',
-                'CustomerName',
-                'DeliveredMatchType'
+            "Attributes": ["CustomerId", "CustomerName", "DeliveredMatchType"],
+            "ImpressionSharePerformanceStatistics": [
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+                "RelativeCtr",
             ],
-            'ImpressionSharePerformanceStatistics': [
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'RelativeCtr'
-            ]
         },
         {
-            'Attributes': [
-                'BidMatchType',
-                'DeviceOS',
-                'Goal',
-                'GoalType',
-                'TopVsOther'
+            "Attributes": [
+                "BidMatchType",
+                "DeviceOS",
+                "Goal",
+                "GoalType",
+                "TopVsOther",
             ],
-            'ImpressionSharePerformanceStatistics': [
-                'AbsoluteTopImpressionRatePercent',
-                'AbsoluteTopImpressionShareLostToBudgetPercent',
-                'AbsoluteTopImpressionShareLostToRankPercent',
-                'AbsoluteTopImpressionSharePercent',
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'ClickSharePercent',
-                'ExactMatchImpressionSharePercent',
-                'ImpressionLostToAdRelevancePercent',
-                'ImpressionLostToBidPercent',
-                'ImpressionLostToBudgetPercent',
-                'ImpressionLostToExpectedCtrPercent',
-                'ImpressionLostToRankAggPercent',
-                'ImpressionLostToRankPercent',
-                'ImpressionSharePercent',
-                'RelativeCtr',
-                'TopImpressionRatePercent',
-                'TopImpressionShareLostToBudgetPercent',
-                'TopImpressionShareLostToRankPercent',
-                'TopImpressionSharePercent'
-            ]
-        }],
-    'AdGroupPerformanceReport': [
+            "ImpressionSharePerformanceStatistics": [
+                "AbsoluteTopImpressionRatePercent",
+                "AbsoluteTopImpressionShareLostToBudgetPercent",
+                "AbsoluteTopImpressionShareLostToRankPercent",
+                "AbsoluteTopImpressionSharePercent",
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+                "ClickSharePercent",
+                "ExactMatchImpressionSharePercent",
+                "ImpressionLostToAdRelevancePercent",
+                "ImpressionLostToBidPercent",
+                "ImpressionLostToBudgetPercent",
+                "ImpressionLostToExpectedCtrPercent",
+                "ImpressionLostToRankAggPercent",
+                "ImpressionLostToRankPercent",
+                "ImpressionSharePercent",
+                "RelativeCtr",
+                "TopImpressionRatePercent",
+                "TopImpressionShareLostToBudgetPercent",
+                "TopImpressionShareLostToRankPercent",
+                "TopImpressionSharePercent",
+            ],
+        },
+    ],
+    "AdGroupPerformanceReport": [
         {
-            'Attributes': [
-                'CustomerId',
-                'CustomerName',
-                'DeliveredMatchType'
+            "Attributes": ["CustomerId", "CustomerName", "DeliveredMatchType"],
+            "ImpressionSharePerformanceStatistics": [
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+                "RelativeCtr",
             ],
-            'ImpressionSharePerformanceStatistics': [
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'RelativeCtr'
-            ]
-        },{
-            'Attributes': [
-                'BidMatchType',
-                'DeviceOS',
-                'Goal',
-                'GoalType',
-                'TopVsOther'
-            ],
-            'ImpressionSharePerformanceStatistics': [
-                'AbsoluteTopImpressionRatePercent',
-                'AbsoluteTopImpressionShareLostToBudgetPercent',
-                'AbsoluteTopImpressionShareLostToRankPercent',
-                'AbsoluteTopImpressionSharePercent',
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'ClickSharePercent',
-                'ExactMatchImpressionSharePercent',
-                'ImpressionLostToAdRelevancePercent',
-                'ImpressionLostToBidPercent',
-                'ImpressionLostToBudgetPercent',
-                'ImpressionLostToExpectedCtrPercent',
-                'ImpressionLostToRankAggPercent',
-                'ImpressionLostToRankPercent',
-                'ImpressionSharePercent',
-                'RelativeCtr',
-                'TopImpressionRatePercent',
-                'TopImpressionShareLostToBudgetPercent',
-                'TopImpressionShareLostToRankPercent',
-                'TopImpressionSharePercent'
-            ]
-        }],
-    'CampaignPerformanceReport': [
+        },
         {
-            'Attributes': [
-                'CustomerId',
-                'CustomerName',
-                'DeliveredMatchType'
+            "Attributes": [
+                "BidMatchType",
+                "DeviceOS",
+                "Goal",
+                "GoalType",
+                "TopVsOther",
             ],
-            'ImpressionSharePerformanceStatistics': [
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'RelativeCtr'
-            ]
-        },{
-            'Attributes': [
-                'BidMatchType',
-                'BudgetAssociationStatus',
-                'BudgetName',
-                'BudgetStatus',
-                'DeviceOS',
-                'Goal',
-                'GoalType',
-                'TopVsOther'
+            "ImpressionSharePerformanceStatistics": [
+                "AbsoluteTopImpressionRatePercent",
+                "AbsoluteTopImpressionShareLostToBudgetPercent",
+                "AbsoluteTopImpressionShareLostToRankPercent",
+                "AbsoluteTopImpressionSharePercent",
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+                "ClickSharePercent",
+                "ExactMatchImpressionSharePercent",
+                "ImpressionLostToAdRelevancePercent",
+                "ImpressionLostToBidPercent",
+                "ImpressionLostToBudgetPercent",
+                "ImpressionLostToExpectedCtrPercent",
+                "ImpressionLostToRankAggPercent",
+                "ImpressionLostToRankPercent",
+                "ImpressionSharePercent",
+                "RelativeCtr",
+                "TopImpressionRatePercent",
+                "TopImpressionShareLostToBudgetPercent",
+                "TopImpressionShareLostToRankPercent",
+                "TopImpressionSharePercent",
             ],
-            'ImpressionSharePerformanceStatistics': [
-                'AbsoluteTopImpressionRatePercent',
-                'AbsoluteTopImpressionShareLostToBudgetPercent',
-                'AbsoluteTopImpressionShareLostToRankPercent',
-                'AbsoluteTopImpressionSharePercent',
-                'AudienceImpressionLostToBudgetPercent',
-                'AudienceImpressionLostToRankPercent',
-                'AudienceImpressionSharePercent',
-                'ClickSharePercent',
-                'ExactMatchImpressionSharePercent',
-                'ImpressionLostToAdRelevancePercent',
-                'ImpressionLostToBidPercent',
-                'ImpressionLostToBudgetPercent',
-                'ImpressionLostToExpectedCtrPercent',
-                'ImpressionLostToRankAggPercent',
-                'ImpressionLostToRankPercent',
-                'ImpressionSharePercent',
-                'TopImpressionRatePercent',
-                'TopImpressionShareLostToBudgetPercent',
-                'TopImpressionShareLostToRankPercent',
-                'TopImpressionSharePercent'
-            ]
-        }],
-    'ProductDimensionPerformanceReport': [{
-        'Attributes': [
-            'AdDistribution',
-            'AdId',
-            'AdStatus',
-            'ClickType',
-            'ClickTypeId',
-            'Goal',
-            'GoalType',
-            'Language',
-            'LocalStoreCode',
-            'Network',
-            'TopVsOther'
-        ],
-        'ImpressionSharePerformanceStatistics': [
-            'AbsoluteTopImpressionSharePercent',
-            'AbsoluteTopImpressionShareLostToBudgetPercent',
-            'AbsoluteTopImpressionShareLostToRankPercent',
-            'AbsoluteTopImpressionSharePercent',
-            'BenchmarkBid',
-            'BenchmarkCtr',
-            'ClickSharePercent',
-            'ImpressionLostToBudgetPercent',
-            'ImpressionLostToRankPercent',
-            'ImpressionSharePercent',
-            'TopImpressionRatePercent',
-            'TopImpressionShareLostToBudgetPercent',
-            'TopImpressionShareLostToRankPercent',
-            'TopImpressionSharePercent'
-        ]
-    }],
-    'ProductPartitionPerformanceReport': [{
-        'Attributes': [
-            'AdDistribution',
-            'AdId',
-            'AdStatus',
-            'BidMatchType',
-            'ClickType',
-            'ClickTypeId',
-            'DeliveredMatchType',
-            'Goal',
-            'GoalType',
-            'Language',
-            'LocalStoreCode',
-            'Network',
-            'TopVsOther'
-        ],
-        'ImpressionSharePerformanceStatistics': [
-            'AbsoluteTopImpressionSharePercent',
-            'BenchmarkBid',
-            'BenchmarkCtr',
-            'ClickSharePercent',
-            'ImpressionLostToBudgetPercent',
-            'ImpressionLostToRankPercent',
-            'ImpressionSharePercent'
-        ]
-    }]
+        },
+    ],
+    "CampaignPerformanceReport": [
+        {
+            "Attributes": ["CustomerId", "CustomerName", "DeliveredMatchType"],
+            "ImpressionSharePerformanceStatistics": [
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+            ],
+        },
+        {
+            "Attributes": [
+                "BidMatchType",
+                "BudgetAssociationStatus",
+                "BudgetName",
+                "BudgetStatus",
+                "DeviceOS",
+                "Goal",
+                "GoalType",
+                "TopVsOther",
+            ],
+            "ImpressionSharePerformanceStatistics": [
+                "AbsoluteTopImpressionRatePercent",
+                "AbsoluteTopImpressionShareLostToBudgetPercent",
+                "AbsoluteTopImpressionShareLostToRankPercent",
+                "AbsoluteTopImpressionSharePercent",
+                "AudienceImpressionLostToBudgetPercent",
+                "AudienceImpressionLostToRankPercent",
+                "AudienceImpressionSharePercent",
+                "ClickSharePercent",
+                "ExactMatchImpressionSharePercent",
+                "ImpressionLostToAdRelevancePercent",
+                "ImpressionLostToBidPercent",
+                "ImpressionLostToBudgetPercent",
+                "ImpressionLostToExpectedCtrPercent",
+                "ImpressionLostToRankAggPercent",
+                "ImpressionLostToRankPercent",
+                "ImpressionSharePercent",
+                "TopImpressionRatePercent",
+                "TopImpressionShareLostToBudgetPercent",
+                "TopImpressionShareLostToRankPercent",
+                "TopImpressionSharePercent",
+            ],
+        },
+    ],
+    "ProductDimensionPerformanceReport": [
+        {
+            "Attributes": [
+                "AdDistribution",
+                "AdId",
+                "AdStatus",
+                "ClickType",
+                "ClickTypeId",
+                "Goal",
+                "GoalType",
+                "Language",
+                "LocalStoreCode",
+                "Network",
+                "TopVsOther",
+            ],
+            "ImpressionSharePerformanceStatistics": [
+                "AbsoluteTopImpressionSharePercent",
+                "AbsoluteTopImpressionShareLostToBudgetPercent",
+                "AbsoluteTopImpressionShareLostToRankPercent",
+                "AbsoluteTopImpressionSharePercent",
+                "BenchmarkBid",
+                "BenchmarkCtr",
+                "ClickSharePercent",
+                "ImpressionLostToBudgetPercent",
+                "ImpressionLostToRankPercent",
+                "ImpressionSharePercent",
+                "TopImpressionRatePercent",
+                "TopImpressionShareLostToBudgetPercent",
+                "TopImpressionShareLostToRankPercent",
+                "TopImpressionSharePercent",
+            ],
+        }
+    ],
+    "ProductPartitionPerformanceReport": [
+        {
+            "Attributes": [
+                "AdDistribution",
+                "AdId",
+                "AdStatus",
+                "BidMatchType",
+                "ClickType",
+                "ClickTypeId",
+                "DeliveredMatchType",
+                "Goal",
+                "GoalType",
+                "Language",
+                "LocalStoreCode",
+                "Network",
+                "TopVsOther",
+            ],
+            "ImpressionSharePerformanceStatistics": [
+                "AbsoluteTopImpressionSharePercent",
+                "BenchmarkBid",
+                "BenchmarkCtr",
+                "ClickSharePercent",
+                "ImpressionLostToBudgetPercent",
+                "ImpressionLostToRankPercent",
+                "ImpressionSharePercent",
+            ],
+        }
+    ],
 }
